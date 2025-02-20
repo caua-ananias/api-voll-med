5be753d92410891c055914ceb32a0d9112787fff
caua-ananias <caua.ananias2004@gmail.com>
2025-02-19 21:00:42 -0400

    Controllers, Enum e DTO feitos

---
+++ src/main/java/medi/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,6 @@
+package medi.voll.api.medico;
+
+import medi.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}

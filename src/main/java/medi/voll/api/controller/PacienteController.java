3549278bf49313083ddcc83366727d9606cc14e9
caua-ananias <caua.ananias2004@gmail.com>
2025-02-19 22:16:50 -0400

    migrations, cadastro cliente

--- src/main/java/medi/voll/api/controller/PacienteController.java
+++ src/main/java/medi/voll/api/controller/PacienteController.java
@@ -1,6 +1,10 @@
 package medi.voll.api.controller;
 
+import jakarta.validation.Valid;
 import medi.voll.api.paciente.DadosCadastroPaciente;
+import medi.voll.api.paciente.Paciente;
+import medi.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
-    public void cadastroPaciente(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+    public void cadastroPaciente(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 }
--- src/main/java/medi/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/medi/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,6 +1,22 @@
 package medi.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import medi.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        String cpf,
+        @Valid
+        @NotNull
+        DadosEndereco endereco) {
 }

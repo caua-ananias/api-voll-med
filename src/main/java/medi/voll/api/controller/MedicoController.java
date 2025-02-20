0bb73c46f13f57f12dac75e5341e182a9c009bfd
caua-ananias <caua.ananias2004@gmail.com>
2025-02-20 11:32:09 -0400

    Listagem de Médicos

--- src/main/java/medi/voll/api/controller/MedicoController.java
+++ src/main/java/medi/voll/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package medi.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import medi.voll.api.medico.DadosCadastroMedico;
+import medi.voll.api.medico.DadosListagemMedico;
 import medi.voll.api.medico.Medico;
 import medi.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listarMedicos() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }

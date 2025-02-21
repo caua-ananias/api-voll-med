c8a9384e84a8eb5a3554d78e8c00e44aef413492
caua-ananias <caua.ananias2004@gmail.com>
2025-02-20 20:42:54 -0400

    Listagem de pacientes, ordenação e paginação

--- src/main/java/medi/voll/api/controller/PacienteController.java
+++ src/main/java/medi/voll/api/controller/PacienteController.java
@@ -2,13 +2,14 @@ package medi.voll.api.controller;
 
 import jakarta.validation.Valid;
 import medi.voll.api.paciente.DadosCadastroPaciente;
+import medi.voll.api.paciente.DadosListagemPacientes;
 import medi.voll.api.paciente.Paciente;
 import medi.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -20,5 +21,10 @@ public class PacienteController {
     @PostMapping
     public void cadastroPaciente(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPacientes> listarPacientes(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPacientes::new);
     }
 }

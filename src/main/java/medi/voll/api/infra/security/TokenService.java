36203b63e9e7234f8a80b1a6c369ad45d3c725a6
caua-ananias <caua.ananias2004@gmail.com>
2025-02-27 11:38:01 -0400

    Autenticação de token, aprimoramento na segurança

--- src/main/java/medi/voll/api/infra/security/TokenService.java
+++ src/main/java/medi/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package medi.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import medi.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,21 @@ public class TokenService {
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
-            throw new RuntimeException("Erro ao gerar token", exception);
+            throw new RuntimeException("Erro ao gerar token!", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido ou expirado!", exception);
         }
     }
 

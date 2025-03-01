fc7c2425a0751902b0b6337484da24ed72991a2b
caua-ananias <caua.ananias2004@gmail.com>
2025-03-01 11:27:06 -0400

    Criação da lógica do agendamento de consultas, validações e exceptions

--- src/main/java/medi/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/medi/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,15 +2,19 @@ package medi.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import medi.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
-public record DadosAgendamentoConsulta(Long idMedico,
+public record DadosAgendamentoConsulta(
+        Long idMedico,
 
-                                       @NotNull
-                                       Long idPaciente,
+        @NotNull
+        Long idPaciente,
 
-                                       @NotNull
-                                       @Future
-                                       LocalDateTime data) {
+        @NotNull
+        @Future
+        LocalDateTime data,
+
+        Especialidade especialidade) {
 }

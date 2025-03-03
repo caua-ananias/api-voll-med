bed1be8215cc246493dd72740f75c38c6d486cfa
caua-ananias <caua.ananias2004@gmail.com>
2025-03-02 21:18:02 -0400

    Criando interface para os Validadores

--- src/main/java/medi/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsulta.java
+++ src/main/java/medi/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsulta.java
@@ -3,9 +3,13 @@ package medi.voll.api.domain.consulta.validacoes;
 import medi.voll.api.domain.ValidacaoException;
 import medi.voll.api.domain.consulta.ConsultaRepository;
 import medi.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorPacienteSemOutraConsulta {
+@Component
+public class ValidadorPacienteSemOutraConsulta implements ValidadorAgendamentoDeConsulta {
 
+    @Autowired
     private ConsultaRepository repository;
 
     public void validar(DadosAgendamentoConsulta dados){
